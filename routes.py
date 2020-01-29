ef3685c96ea52d99a538d5a0d5fbafcc698e89a5
David Wilkins <wilkidav@gmail.com>
2020-01-29 02:21:18 -0500

    through section 4

--- routes.py
+++ routes.py
@@ -1,15 +1,39 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{os.environ['DB_PASSWORD']}@localhost:5432/learningflask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+app.secret_key = "development-key"
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+  return render_template("about.html")
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+  form = SignupForm()
+
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      return 'Success!'
+
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
